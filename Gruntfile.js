529b6fa43c096c962288735bf6f07acc58c910f5
Nico Rehwaldt <git_nikku@nixis.de>
2017-11-23 14:53:53 +0100

    chore(tests): configure TEST_BROWSERS in karma config

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,6 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
 
-  /* global process */
-
-  // configures browsers to run test against
-  // any of [ 'PhantomJS', 'Chrome', 'Firefox', 'IE']
-  var TEST_BROWSERS = ((process.env.TEST_BROWSERS || '').replace(/^\s+|\s+$/, '') || 'PhantomJS').split(/\s*,\s*/g);
-
   // project configuration
   grunt.initConfig({
 
@@ -38,13 +32,9 @@ module.exports = function(grunt) {
       },
       single: {
         singleRun: true,
-        autoWatch: false,
-
-        browsers: TEST_BROWSERS
+        autoWatch: false
       },
-      unit: {
-        browsers: TEST_BROWSERS
-      }
+      unit: { }
     },
 
     release: {
