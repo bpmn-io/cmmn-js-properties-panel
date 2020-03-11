f3b16a9f9dccde7613de88243a0eaa6809c14880
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-03-11 12:32:19 +0100

    fix(properties-panel): make sure to always paste as plain text
    Related to https://github.com/bpmn-io/bpmn-js-properties-panel/issues/265

--- test/spec/PropertiesPanelSpec.js
+++ test/spec/PropertiesPanelSpec.js
@@ -253,5 +253,10 @@ describe('properties-panel', function() {
   });
 
 
+  describe('listeners', function() {
 
+    // Cannot be tested due to paste events not affecting document's contents per default.
+    // Cf. https://developer.mozilla.org/en-US/docs/Web/API/Element/paste_event
+    it('should paste to [contenteditable] as plain text');
+  });
 });
